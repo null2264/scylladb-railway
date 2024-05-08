8b780822f9d8210708a37dfc9fc1bdda56dc52b5
Ahmad Ansori Palembani <palembani@gmail.com>
2024-05-08 09:44:36 +0700

    enhance: Experimental refactor

---
+++ wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SMP="${SMP:-2}"
+MEM="${MEM:-2G}"
+OVERPROV="${OVERPROV:-1}"
+API_ADDR="${API_ADDR:-::}"
+LISTEN_ADDR="${LISTEN_ADDR:-::}"
+
+[ "$1" = "scylladb" ] && shift
+
+/docker-entrypoint.py \
+	--smp "$SMP" \
+	--memory "$MEM" \
+	--overprovisioned "$OVERPROV" \
+	--api-address "$API_ADDR" \
+	--listen-address "$LISTEN_ADDR" \
+	--rpc-address "$LISTEN_ADDR" \
+	--alternator-address "$LISTEN_ADDR"
